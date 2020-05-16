ae1f648444f156bd2fe49a4a1e2bfd691f4eb88e
mmenchon4 <mmenchon4@gmail.com>
2020-05-16 19:13:40 -0300

    add main.py and .gitignore

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2 as cv
+
+imgWillOriginal = cv.imread("ImagenesWill/Will(Original).bmp")
+imgWill1 = cv.imread("ImagenesWill/Will(Original).bmp")
+imgWill2 = cv.imread("ImagenesWill/Will(Original).bmp")
+imgWill3 = cv.imread("ImagenesWill/Will(Original).bmp")
+imgWill4 = cv.imread("ImagenesWill/Will(Original).bmp")
+imgWill5 = cv.imread("ImagenesWill/Will(Original).bmp")
+imgWillCanal2 = cv.imread("ImagenesWill/Will(Original).bmp")
+imgWillCanal8 = cv.imread("ImagenesWill/Will(Original).bmp")
+imgWillCanal10 = cv.imread("ImagenesWill/Will(Original).bmp")
+imgWillEj2 = cv.imread("ImagenesWill/Will(Original).bmp")
+# correlacionCruzada  = cv.templateMachine(img)
+
+print( imgWillOriginal )
+# print( correlacionCruzada)
