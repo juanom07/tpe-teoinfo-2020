705679320224940a472844b0eefd08c871e30433
mmenchon4 <mmenchon4@gmail.com>
2020-05-18 00:51:01 -0300

    add Correlacion cruzada

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+import math
 
 imgWillOriginal = cv.imread("ImagenesWill/Will(Original).bmp")
 imgWill1 = cv.imread("ImagenesWill/Will_1.bmp")
@@ -39,3 +40,50 @@ hist.show()
 
 his2 = obtener_histograma(imgWill5)
 his2.show()
+
+
+def getMedia(imagen):
+    media = 0
+    for i in range(imagen.shape[0]):
+        for j in range(imagen.shape[1]):
+            media += imagen[i,j,0]
+    media = media/(imagen.shape[0]*imagen.shape[1])
+    return media
+
+
+def getDesvio(imagen, media):
+    desvio = 0
+    for i in range(imagen.shape[0]):
+        for j in range(imagen.shape[1]):
+            desvio += ((imagen[i,j,0] - media) * (imagen[i,j,0] - media))
+    desvio = desvio/(imagen.shape[0]*imagen.shape[1])
+    return math.sqrt(desvio)
+
+def getCorrelacionCruzada(imagenA, imagenB, mediaA, mediaB):
+    sxy = 0
+    for i in range(imagenA.shape[0]):
+        for j in range(imagenA.shape[1]):
+            sxy += ((imagenA[i,j,0] - mediaA) * (imagenB[i,j,0] - mediaB))
+    sxy = sxy/(imagenA.shape[0]*imagenA.shape[1])
+    return sxy
+
+def getFactorCorrelacion(imagenA, imagenB):
+    mediaA = getMedia(imagenA)
+    mediaB = getMedia(imagenB)
+    desvioA = getDesvio(imagenA, mediaA)
+    desvioB = getDesvio(imagenB, mediaB)
+    sxy = getCorrelacionCruzada(imagenA, imagenB, mediaA, mediaB)
+    return sxy/(desvioA*desvioB)
+
+res = cv.matchTemplate(imgWillOriginal,imgWill1,cv.TM_CCOEFF_NORMED)
+print(res)
+r = getFactorCorrelacion(imgWillOriginal, imgWill1)
+print(r)
+r = getFactorCorrelacion(imgWillOriginal, imgWill2)
+print(r)
+r = getFactorCorrelacion(imgWillOriginal, imgWill3)
+print(r)
+r = getFactorCorrelacion(imgWillOriginal, imgWill4)
+print(r)
+r = getFactorCorrelacion(imgWillOriginal, imgWill5)
+print(r)
